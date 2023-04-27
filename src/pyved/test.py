dc4ef72bb2a5aa05d7dae488fb79c3bbdb80dd25
wkta-tom <tiwaszko@gmail.com>
2023-04-27 18:07:56 +0200

    expand basic test so it checks if pygame works ok

--- src/pyved/test.py
+++ src/pyved/test.py
@@ -1,2 +1,25 @@
+import pygame
+from dataclasses import dataclass
 
+
+@dataclass
+class Person:
+    name: str
+    age: int
+    height: float
+    email: str
+
+
+mec = Person('thomas', 99, 1.85, 'thomas.iw@kata.games')
 print('hello world')
+gameover = False
+surf = pygame.display.set_mode((640, 480))
+
+while not gameover:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            gameover = True
+    surf.fill('pink')
+    pygame.display.flip()
+
+print('bye', mec.name, 'see you when you turn', mec.age+1)
