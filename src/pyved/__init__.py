e03cb3900e4c5cd33d946d5af23db9d44a330971
wkta-tom <tiwaszko@gmail.com>
2023-05-14 12:13:38 +0200

    update gitignore +Add: engine elem access is ok from pyved

--- src/pyved/__init__.py
+++ src/pyved/__init__.py
@@ -6,4 +6,25 @@ Visit https://kata.games to learn more!
 
 __version__ = '0.0.6'
 
-from ._ecs_pattern import entity, component, EntityManager, System, SystemManager
+
+import pyved_engine as _engine
+# _engine.bootstrap_e()
+
+from pyved_engine import component, entity, EntityManager, System, SystemManager
+from pyved_engine.core.events import EvManager, EvListener
+from pyved_engine import game_events_enum, get_ev_manager, init, quit, get_surface, flip
+
+
+def pygame_proxy():
+    _engine.bootstrap_e()
+    return _engine.pygame
+
+
+# lists what should be interpreted by import *
+# this overrides the default of hiding everything that begins with an underscore
+__all__ = [
+    component, entity, EntityManager, System, SystemManager,
+    EvManager, EvListener,
+    game_events_enum, get_ev_manager, init, quit, get_surface, flip,
+    pygame_proxy
+]
